d77a5fb5ab72a46b78f40445aa40ed022117b06e
rashid44 <rashid2k22@gmail.com>
2021-07-07 12:48:58 +0600

    First Commit

---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+    return (
+        <div>
+            <h2>My Event</h2>
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEventPage() {
+    return (
+        <div>
+            <h2>Add Events</h2>
+        </div>
+    )
+}
